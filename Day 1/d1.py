6ee3e9238a63011c6acb1974d466a4e086a9caaa
strogera <26325577+strogera@users.noreply.github.com>
2020-11-30 20:29:39 +0200

    Preperation for Day 1

---
+++ Day 1/d1.py
@@ -0,0 +1,16 @@
+def partOne():
+    with open("input.txt", "r") as inputFile:
+        for line in inputFile:
+            continue
+
+
+def partTwo():
+    with open("input.txt", "r") as inputFile:
+        for line in inputFile:
+            continue
+
+
+print("Answer for part one: ")
+partOne()
+print("Answer for part two: ")
+partTwo()
