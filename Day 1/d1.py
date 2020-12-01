b1a832329b951b1e257be08e9d137a6ac729bdce
strogera <26325577+strogera@users.noreply.github.com>
2020-12-01 07:37:12 +0200

    Day 1

--- Day 1/d1.py
+++ Day 1/d1.py
@@ -1,14 +1,24 @@
 def partOne():
     with open("input.txt", "r") as inputFile:
+        sumDict={}
         for line in inputFile:
-            continue
-
+            sumDict[int(line)]=2020-int(line)
+        for x in sumDict:
+           if sumDict[x] in sumDict:
+               print(x*sumDict[x]) 
+               return
 
 def partTwo():
     with open("input.txt", "r") as inputFile:
+        sumDict={}
         for line in inputFile:
-            continue
-
+            sumDict[int(line)]=2020-int(line)
+        for x in sumDict:
+            for y in sumDict:
+                if x!=y:
+                    if sumDict[x]-y in sumDict:
+                        print(x*y*(sumDict[x]-y)) 
+                        return
 
 print("Answer for part one: ")
 partOne()
