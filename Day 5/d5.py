7be72156b91ec61e10f3aec514fa21384319821c
strogera <26325577+strogera@users.noreply.github.com>
2020-12-05 12:16:32 +0200

    Day 5 refactoring

--- Day 5/d5.py
+++ Day 5/d5.py
@@ -7,20 +7,34 @@ def splitBinary(start, end, splitType):
         start+=math.ceil((end-start)/2)
     return (start, end)
 
+def getRow(codes):
+    rowsStart=0
+    rowsEnd=127
+    for splitType in codes:
+        (rowsStart, rowsEnd)=splitBinary(rowsStart, rowsEnd, splitType)
+    return rowsStart
+
+def getColumn(codes):
+    columnsStart=0
+    columnsEnd=7
+    for splitType in codes:
+        columnsStart, columnsEnd=splitBinary(columnsStart, columnsEnd, splitType)
+    return columnsStart
+
+def getSeatId(row, column):
+    return row*8+column
+
+def getMissingSeatId(seatIds):
+    return set(range(min(seatIds), max(seatIds)+1)).difference(seatIds).pop()
+
 def partOne():
     with open("input.txt", "r") as inputFile:
         seatIds=[]
         for line in inputFile:
             line=line.strip()
-            rowsStart=0
-            rowsEnd=127
-            for splitType in line[:-3]:
-                (rowsStart, rowsEnd)=splitBinary(rowsStart, rowsEnd, splitType)
-            columnsStart=0
-            columnsEnd=7
-            for splitType in line[-3:]:
-                columnsStart, columnsEnd=splitBinary(columnsStart, columnsEnd, splitType)
-            seatIds.append(rowsStart*8+columnsStart)
+            row=getRow(line[:-3])
+            column=getColumn(line[-3:])
+            seatIds.append(getSeatId(row, column))
         return max(seatIds)
 
 
@@ -29,16 +43,10 @@ def partTwo():
         seatIds=[]
         for line in inputFile:
             line=line.strip()
-            rowsStart=0
-            rowsEnd=127
-            for splitType in line[:-3]:
-                (rowsStart, rowsEnd)=splitBinary(rowsStart, rowsEnd, splitType)
-            columnsStart=0
-            columnsEnd=7
-            for splitType in line[-3:]:
-                columnsStart, columnsEnd=splitBinary(columnsStart, columnsEnd, splitType)
-            seatIds.append(rowsStart*8+columnsStart)
-        return set(range(min(seatIds), max(seatIds)+1)).difference(seatIds).pop()
+            row=getRow(line[:-3])
+            column=getColumn(line[-3:])
+            seatIds.append(getSeatId(row, column))
+        return getMissingSeatId(seatIds)
 
 
 print("Answer for part 1: ")
