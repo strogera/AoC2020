b7310fa8098e2b6339d748ac01e44cc3759a0a9c
strogera <26325577+strogera@users.noreply.github.com>
2020-12-15 14:31:36 +0200

    Day 15 alternative solution

--- Day 15/d15.py
+++ Day 15/d15.py
@@ -2,28 +2,46 @@ def partOne(iterations):
     with open("input.txt", "r") as inputFile:
         elems=[int(x) for x in inputFile.read().strip().split(',')]
 
-        summs=[-1]*(iterations + 1)
-        rev = [(-1, -1)]*(iterations + 1)
+        numberAtTurn=[-1]*(iterations + 1)
+        previouslySeen = [(-1, -1)]*(iterations + 1)
 
         for i, num in enumerate(elems):
-            summs[i+1] = num
-            rev[num] = (i + 1, i + 1)
+            numberAtTurn[i+1] = num
+            previouslySeen[num] = (i + 1, i + 1)
 
         for i in range(len(elems)+1, iterations+1):
-            prev = summs[i-1]
-            if prev == -1 or rev[prev][0] == -1 or rev[prev][1] == -1 :
+            previousNumber = numberAtTurn[i-1]
+            if previousNumber == -1 or previouslySeen[previousNumber][0] == -1 or previouslySeen[previousNumber][1] == -1 :
             #first time or once
-                summs[i] = 0
-                rev[0] = (rev[0][1], i)
+                numberAtTurn[i] = 0
+                previouslySeen[0] = (previouslySeen[0][1], i)
             else:
-                summs[i] = rev[prev][1] - rev[prev][0]
-                rev[summs[i]] = (rev[summs[i]][1], i)            
+                numberAtTurn[i] = previouslySeen[previousNumber][1] - previouslySeen[previousNumber][0]
+                previouslySeen[numberAtTurn[i]] = (previouslySeen[numberAtTurn[i]][1], i)            
 
-        return summs[iterations]
+        return numberAtTurn[iterations]
                 
+
+def partOneAlt(iterations):
+    #Alternative solution without using too much memory
+    with open("input.txt", "r") as inputFile:
+        nums={int(x): i+1 for i, x in enumerate(inputFile.read().strip().split(','))}
+        turn=len(nums)+1
+        numCurrent=0
+        while(turn < iterations):
+            numPrev=nums.get(numCurrent)
+            nums[numCurrent] = turn
+            if numPrev:
+                numCurrent = turn - numPrev 
+            else:
+                numCurrent=0
+            turn +=1
+        return numCurrent
 
 
 print("Answer for part 1: ")
 print(partOne(2020))
+#print(partOneAlt(2020))
 print("Answer for part 2: ")
 print(partOne(30000000))
+#print(partOneAlt(30000000))
