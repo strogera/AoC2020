5b789b70b0d881fcb5c0560a2f5dd4c74a806bd3
strogera <26325577+strogera@users.noreply.github.com>
2020-12-22 14:58:07 +0200

    Day 17 small changes

--- Day 17/d17.py
+++ Day 17/d17.py
@@ -1,18 +1,17 @@
-from itertools import combinations  
 
 def getNeighbors(x, y, z):
-    neighbors=[]
+    neighbors = set()
     for xi in range(x-1, x+2):
         for yi in range(y-1, y+2):
             for zi in range(z-1, z+2):
                 if xi == x and yi == y and zi==z:
                     continue
-                neighbors.append((xi, yi, zi))
+                neighbors.add((xi, yi, zi))
     return neighbors
 
 
 def getActiveNeighbors(neighbors, active):
-    return [(xi, yi, zi) for xi, yi, zi in neighbors if (xi, yi, zi) in active]
+    return active.intersection(neighbors)
 
 
 def simulate(active):
@@ -49,19 +48,15 @@ def partOne(numOfSimulations):
 
 
 def getNeighbors4d(x, y, z, w):
-    neighbors=[]
+    neighbors = set()
     for xi in range(x-1, x+2):
         for yi in range(y-1, y+2):
             for zi in range(z-1, z+2):
                 for wi in range(w-1, w+2):
                     if xi == x and yi == y and zi == z and wi == w:
                         continue
-                    neighbors.append((xi, yi, zi, wi))
+                    neighbors.add((xi, yi, zi, wi))
     return neighbors
-
-
-def getActiveNeighbors4d(neighbors, active):
-    return [(xi, yi, zi, wi) for xi, yi, zi, wi in neighbors if (xi, yi, zi, wi) in active]
 
 
 def simulate4d(active):
@@ -69,12 +64,12 @@ def simulate4d(active):
     discovered=set()
     for (x, y, z, w) in active:
         neighbors=getNeighbors4d(x, y, z, w)
-        activeNeighbors= getActiveNeighbors4d(neighbors, active)
+        activeNeighbors= getActiveNeighbors(neighbors, active)
         if len(activeNeighbors) == 2 or len(activeNeighbors) == 3:
             newActive.add((x, y, z, w))
         for nx, ny, nz, nw in neighbors:
             if (nx, ny, nz, nw) not in discovered:
-                if len(getActiveNeighbors4d(getNeighbors4d(nx, ny, nz, nw), active)) == 3:
+                if len(getActiveNeighbors(getNeighbors4d(nx, ny, nz, nw), active)) == 3:
                     newActive.add((nx, ny, nz, nw))
                     discovered.add((nx, ny, nz, nw))
     return newActive
