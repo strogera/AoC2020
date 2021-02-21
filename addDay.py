7c6a772f4e842740d847ca4b777edf2b1915e04b
strogera <26325577+strogera@users.noreply.github.com>
2021-02-21 13:11:51 +0200

    first day fix

--- addDay.py
+++ addDay.py
@@ -3,7 +3,7 @@ from os import makedirs
 from shutil import copyfile
 
 dayFolders=[int(x.replace('Day ', '')) for x in listdir('./') if 'Day ' in x]
-lastDay=max(dayFolders)
+lastDay=max(dayFolders) if dayFolders else 0
 newDay='Day ' + str(lastDay+1)
 makedirs(newDay)
 copyfile('./template.py', './'+newDay+'/d'+str(lastDay+1)+'.py')
